0855706e9f620baf7510afd15c012d4037e3bd10
nicoll-douglas <dev.nicoll.douglas@gmail.com>
2024-12-05 18:06:46 +0000

    refactor: books actions menu

--- src/features/search-books/components/BookPreview/BookActionsMenu.tsx
+++ src/features/search-books/components/BookPreview/BookActionsMenu.tsx
@@ -7,9 +7,14 @@ export default function BookActionsMenu({ volumeId }: { volumeId: string }) {
   const contentId = `action-content-${volumeId}`;
   const containerId = `action-container-${volumeId}`;
   const buttonId = `action-btn-${volumeId}`;
+  const currentlyReadingId = `cr-${volumeId}`;
+  const wantToReadId = `wtr-${volumeId}`;
+  const haveReadId = `hr-${volumeId}`;
 
-  const { onKeyDown, onClose, onToggle, isOpen, refs } =
-    useMenu<HTMLButtonElement>();
+  const { onKeyDown, onClose, onToggle, isOpen, refs } = useMenu<
+    HTMLInputElement,
+    HTMLButtonElement
+  >();
   useClickAway(`#${containerId}`, onClose);
 
   return (
@@ -27,40 +32,54 @@ export default function BookActionsMenu({ volumeId }: { volumeId: string }) {
         <A.menu.Content
           id={contentId}
           aria-labelledby={buttonId}
-          className="w-60 gap-1 shadow-sm right-6 bottom-6"
+          className="p-3 shadow-sm right-6 bottom-6 text-lg"
+          role="dialog"
+          aria-modal={isOpen}
         >
-          <A.menu.Item>
+          <form className="flex gap-2 flex-col w-full">
+            <div className="flex gap-3 items-center">
+              <A.form.Label
+                htmlFor={currentlyReadingId}
+                className="mb-0 text-nowrap"
+              >
+                Currently Reading
+              </A.form.Label>
+              <A.form.Checkbox
+                id={currentlyReadingId}
+                name="currentlyReading"
+                className="ml-auto"
+                ref={refs.firstFocus}
+              />
+            </div>
+            <div className="flex gap-3 items-center">
+              <A.form.Label htmlFor={wantToReadId} className="mb-0">
+                Want to Read
+              </A.form.Label>
+              <A.form.Checkbox
+                id={wantToReadId}
+                name="wantToRead"
+                className="ml-auto"
+              />
+            </div>
+            <div className="flex gap-3 items-center">
+              <A.form.Label htmlFor={haveReadId} className="mb-0">
+                Have Read
+              </A.form.Label>
+              <A.form.Checkbox
+                id={haveReadId}
+                name="haveRead"
+                className="ml-auto"
+              />
+            </div>
             <A.Button
-              onClick={onClose}
-              ref={refs.firstFocus}
-              className="px-4 flex-1 min-w-fit"
               size="sm"
-              rightIcon="/icons/watch-icon.svg"
-            >
-              Currently Reading
-            </A.Button>
-          </A.menu.Item>
-          <A.menu.Item>
-            <A.Button
-              onClick={onClose}
-              className="px-4 flex-1 min-w-fit"
-              size="sm"
-              rightIcon="/icons/add-icon.svg"
-            >
-              Want to Read
-            </A.Button>
-          </A.menu.Item>
-          <A.menu.Item>
-            <A.Button
-              onClick={onClose}
-              className="px-4 flex-1 min-w-fit"
-              size="sm"
+              type="submit"
               ref={refs.lastFocus}
-              rightIcon="/icons/tick.svg"
+              onClick={onClose}
             >
-              Have Read
+              Save
             </A.Button>
-          </A.menu.Item>
+          </form>
         </A.menu.Content>
       )}
     </A.menu.Container>
